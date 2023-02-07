7b44ab91deac3c80045bb1fcb627335907f93c1a
Sathish1163 <sathishkumar.b@colanonline.com>
2023-02-07 16:52:02 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Idcardmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
